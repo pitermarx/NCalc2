bc7969c9b1e3e142bbe97fbd02fb897c70d3ae1d
pitermarx <pedrohmarques@gmail.com>
2015-04-02 09:27:46 +0100

    updated test scripts

---
+++ Scripts/nunit.bat
@@ -0,0 +1,1 @@
+nunit-console /noshadow Evaluant.Calculator.Tests\bin\%1\NCalc.Tests.dll
---
+++ Scripts/test_Script.ps1
@@ -0,0 +1,24 @@
+# Variables
+$config = if ($env:CONFIGURATION) { $env:CONFIGURATION } else { "Debug" }
+
+Write-Host "----------------------"
+Write-Host "   Running Coverage   "
+Write-Host "----------------------"
+& .\packages\OpenCover.4.5.3522\OpenCover.Console.exe -register:user `
+-returntargetcode "-filter:+[NCalc*]*" "-target:.\scripts\nunit.bat" `
+"-targetargs: $config"
+
+Write-Host "----------------------------"
+Write-Host "Sending data to Coveralls.io"
+Write-Host "----------------------------"
+& .\packages\coveralls.net.0.5.0\csmacnz.Coveralls.exe --opencover -i results.xml `
+--repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT `
+--commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor pitermarx `
+--commitEmail pedrohmarques@gmail.com --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE `
+--jobId $env:APPVEYOR_JOB_ID
+
+Write-Host "----------------------"
+Write-Host "Generating HTML Report"
+Write-Host "----------------------"
+& .\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe -reports:results.xml `
+-targetdir:Reports\Latest -sourcedirs:Evaluant.Calculator -historydir:Reports\History
