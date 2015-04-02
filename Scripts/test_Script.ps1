345d7eedacf7d12e9b3b32e922218ddc402a8311
pitermarx <pedrohmarques@gmail.com>
2015-04-02 09:36:30 +0100

    revert change

--- Scripts/test_Script.ps1
+++ Scripts/test_Script.ps1
@@ -1,19 +1,17 @@
-cd scripts
-
 # Variables
 $config = if ($env:CONFIGURATION) { $env:CONFIGURATION } else { "Debug" }
 
 Write-Host "----------------------"
 Write-Host "   Running Coverage   "
 Write-Host "----------------------"
-& ..\packages\OpenCover.4.5.3522\OpenCover.Console.exe -register:user `
--returntargetcode "-filter:+[NCalc*]*" "-target:nunit.bat" `
+& .\packages\OpenCover.4.5.3522\OpenCover.Console.exe -register:user `
+-returntargetcode "-filter:+[NCalc*]*" "-target:.\scripts\nunit.bat" `
 "-targetargs: $config"
 
 Write-Host "----------------------------"
 Write-Host "Sending data to Coveralls.io"
 Write-Host "----------------------------"
-& ..\packages\coveralls.net.0.5.0\csmacnz.Coveralls.exe --opencover -i results.xml `
+& .\packages\coveralls.net.0.5.0\csmacnz.Coveralls.exe --opencover -i results.xml `
 --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT `
 --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor pitermarx `
 --commitEmail pedrohmarques@gmail.com --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE `
@@ -22,5 +20,5 @@ Write-Host "----------------------------"
 Write-Host "----------------------"
 Write-Host "Generating HTML Report"
 Write-Host "----------------------"
-& ..\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe -reports:results.xml `
+& .\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe -reports:results.xml `
 -targetdir:Reports\Latest -sourcedirs:Evaluant.Calculator -historydir:Reports\History
